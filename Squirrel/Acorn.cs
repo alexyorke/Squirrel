5b979c40008c7a8c4806e21731f5f1ea0903cffe
SmallJoker <mk939@ymail.com>
2015-07-23 11:34:17 +0200

    Huge rewrite - Increase the speed incredibly by removing all List<Tuple<>> - Make C# 2010 Express compatible - Remove Json and Rabbit - Convert spaces to tabs

--- Squirrel/Acorn.cs
+++ Squirrel/Acorn.cs
@@ -2,77 +2,75 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 
 namespace Decagon.EE
 {
-    /// <summary>
-    /// The acorn class.
-    /// </summary>
-    public class Acorn
-    {
+	/// <summary>
+	/// The acorn class.
+	/// </summary>
+	public class Acorn
+	{
 
-        /// <summary>
-        /// Loads the blocks.
-        /// </summary>
-        /// <returns>Dictionary containing the blocks with their respective ids and colors.</returns>
-        /// <exception cref="InvalidDataException">
-        /// The blocks did not download correctly.
-        /// or
-        /// The blocks are not in the correct format.
-        /// </exception>
-        public static Dictionary<string,Color> LoadBlocks()
-        {
-            // if the acorn file does not exist...
-            string text = null;
-            using (var wc = new System.Net.WebClient())
-                text = wc.DownloadString("https://raw.githubusercontent.com/Tunous/EEBlocks/master/Colors.txt");
+		/// <summary>
+		/// Loads the blocks.
+		/// </summary>
+		/// <returns>Dictionary containing the blocks with their respective ids and colors.</returns>
+		/// <exception cref="InvalidDataException">
+		/// The blocks did not download correctly.
+		/// or
+		/// The blocks are not in the correct format.
+		/// </exception>
+		public static Dictionary<string, Color> LoadBlocks()
+		{
+			// if the acorn file does not exist...
+			string text = null;
+			using (var wc = new System.Net.WebClient())
+				text = wc.DownloadString("https://raw.githubusercontent.com/Tunous/EEBlocks/master/Colors.txt");
 
-            if (text == null)
-            {
-                throw new InvalidDataException("The blocks did not download correctly.");
-            }
+			if (text == null)
+				throw new InvalidDataException("The blocks did not download correctly.");
 
-            if (!text.Contains("ID: ") || !text.Contains("Mapcolor:"))
-            {
-                throw new InvalidDataException("The blocks are not in the correct format.");
-            }
+			if (!text.StartsWith("ID: 0 Mapcolor: "))
+				throw new InvalidDataException("The blocks are not in the correct format.");
 
-            text = text.Replace("ID: ", "").Replace(" Mapcolor: ", ",");
+			text = text.Replace('\r', ' ');
 
-            var blocks = text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+			Dictionary<string, Color> blockDict = new Dictionary<string, Color>();
 
-            var blockIDs = new List<string>();
-            var blockColors = new List<Color>();
-            foreach (var block in blocks)
-            {
-                string[] tokens = block.Split(new string[] { "," }, StringSplitOptions.None);
-                blockIDs.Add(tokens[0]);
-                blockColors.Add(UIntToColor(Convert.ToUInt32(tokens[1])));
-            }
+			string[] lines = text.Split('\n');
+			for (int i = 0; i < lines.Length; i++) {
+				if (lines[i].Length < 15)
+					continue;
 
-            Dictionary<string,Color> blockDict = blockIDs.Zip(blockColors, (s, i) => new { s, i })
-                          .ToDictionary(item => item.s, item => item.i);
+				string[] line = lines[i].Split(' ');
+				if (line.Length < 4 || line[0] != "ID:" || line[2] != "Mapcolor:")
+					throw new InvalidDataException("Incorrect block color format.");
 
-            System.IO.StreamWriter file = new System.IO.StreamWriter("blocks.acorn");
-            file.Write(Newtonsoft.Json.JsonConvert.SerializeObject(blockDict));
-            file.Close();
+				uint u32color = Convert.ToUInt32(line[3]);
+				if (u32color == 0)
+					continue;
 
-            return blockDict;
-        }
+				blockDict.Add(line[1], UIntToColor(u32color));
+			}
 
-        /// <summary>
-        /// Converts a UInt to a Color object.
-        /// </summary>
-        /// <param name="color">The color.</param>
-        /// <returns>Color object</returns>
-        public static Color UIntToColor(uint color)
-        {
-            byte a = (byte)(color >> 24);
-            byte r = (byte)(color >> 16);
-            byte g = (byte)(color >> 8);
-            byte b = (byte)(color >> 0);
-            return Color.FromArgb(a, r, g, b);
-        }
-    }
+			return blockDict;
+		}
+
+		/// <summary>
+		/// Converts a UInt to a Color object.
+		/// </summary>
+		/// <param name="color">The color.</param>
+		/// <returns>Color object</returns>
+		public static Color UIntToColor(uint color, bool remove_alpha = true)
+		{
+			int a = (byte)(color >> 24),
+				r = (byte)(color >> 16),
+				g = (byte)(color >> 8),
+				b = (byte)(color >> 0);
+
+			if (remove_alpha)
+				a = a > 128 ? 0xFF : 0;
+			return Color.FromArgb(a, r, g, b);
+		}
+	}
 }
--- Squirrel/Program.cs
+++ Squirrel/Program.cs
@@ -1,174 +1,167 @@
 ﻿using PlayerIOClient;
 using System.Threading;
-using Rabbit;
 using System.Drawing;
+
 namespace Decagon.EE
 {
-    using nQuant;
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Drawing.Imaging;
-    class Program
-    {
-        /// <summary>
-        /// The stopwatch
-        /// </summary>
-        static Stopwatch stopwatch = new Stopwatch();
-        /// <summary>
-        /// The wasted_seconds
-        /// </summary>
-        static Stopwatch wasted_seconds = new Stopwatch();
-        /// <summary>
-        /// The global connection
-        /// </summary>
-        static Connection globalConn = null;
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Drawing.Imaging;
+	class Program
+	{
+		/// <summary>
+		/// The wasted_seconds
+		/// </summary>
+		static Stopwatch wasted_seconds = new Stopwatch();
+		/// <summary>
+		/// The global connection
+		/// </summary>
+		static Connection globalConn = null;
 
-        /// <summary>
-        /// The block dictionary
-        /// </summary>
-        public static Dictionary<string, Color> blockDict = new Dictionary<string, Color>();
-        /// <summary>
-        /// The world identifier
-        /// </summary>
-        static string worldID = "PW5WNPqd3ia0I";
+		/// <summary>
+		/// The world identifier
+		/// </summary>
+		static string worldID = "PWL2NjNOdhbEI";
 
-        static void Main(string[] args)
-        {
-            // Load the blocks into memory
-            blockDict = Acorn.LoadBlocks();
+		static bool LOAD_FROM_BIGDB = true;
+		static bool generating_minimap;
 
-            // Log on
-            Client conn = new RabbitAuth().LogOn("everybody-edits-su9rn58o40itdbnw69plyw", Config.Email, Config.Password);
-            while (conn.ConnectUserId == "") { Thread.Sleep(100); } // wasteful
+		static void Main(string[] args)
+		{
+			wasted_seconds.Start();
+			// Log on
+			Client conn = PlayerIO.QuickConnect.SimpleConnect("everybody-edits-su9rn58o40itdbnw69plyw", Config.Email, Config.Password);
 
-            Console.WriteLine("Connected");
+			Console.WriteLine("Connected");
+			generating_minimap = true;
+			if (LOAD_FROM_BIGDB) {
+				DatabaseObject obj = conn.BigDB.Load("Worlds", worldID);
+				if (obj.ExistsInDatabase)
+					FromDatabaseObject(obj);
+				else
+					Console.WriteLine("Error: Unknown WorldID");
+			} else {
+				conn.Multiplayer.JoinRoom(worldID, null, delegate(Connection connection) {
+					connection.OnMessage += Connection_OnMessage;
+					globalConn = connection;
+					connection.Send("init");
+				});
+			}
 
-            stopwatch.Start();
-            conn.Multiplayer.JoinRoom(worldID, null, delegate (Connection connection)
-            {
-                connection.OnMessage += Connection_OnMessage;
-                globalConn = connection;
-                connection.Send("init");
+			while(generating_minimap)
+				Thread.Sleep(10);
 
-            });
-            
-            DatabaseObject obj = conn.BigDB.Load("Worlds", worldID);
-            FromDatabaseObject(obj);
-            Thread.Sleep(Timeout.Infinite);
-        }
+			wasted_seconds.Stop();
 
+			Console.WriteLine("Generated minimap in " + wasted_seconds.Elapsed.Milliseconds + " ms");
+			Console.ReadKey(false);
+		}
 
-        /// <summary>
-        /// Extracts the world from the BigDB database.
-        /// </summary>
-        /// <param name="obj">The object.</param>
-        public static void FromDatabaseObject(DatabaseObject obj)
-        {
-            var width = obj.GetInt("width", 200);
-            var height = obj.GetInt("height", 200);
-            var worldData = obj.GetArray("worlddata");
-            if (worldData != null)
-            {
-                UnserializeFromComplexObject(worldData,width,height);
-            }
-        }
+		/// <summary>
+		/// Extracts the world from the BigDB database.
+		/// </summary>
+		/// <param name="obj">The object.</param>
+		public static void FromDatabaseObject(DatabaseObject obj)
+		{
+			var width = obj.GetInt("width", 200);
+			var height = obj.GetInt("height", 200);
+			if (!obj.Contains("worlddata"))
+				Console.WriteLine("Error: No world data available");
 
-        /// <summary>
-        /// Unserializes the BigDB database world object.
-        /// </summary>
-        /// <param name="worlddata">The world data.</param>
-        /// <param name="width">The width of the world.</param>
-        /// <param name="height">The height of the world.</param>
-        public static void UnserializeFromComplexObject(DatabaseArray worlddata,int width,int height)
-        {
+			UnserializeFromComplexObject(obj.GetArray("worlddata"), width, height);
+		}
 
-            Minimap minimap = new Minimap();
-            minimap.width = width;
-            minimap.height = height;
+		/// <summary>
+		/// Unserializes the BigDB database world object.
+		/// </summary>
+		/// <param name="worlddata">The world data.</param>
+		/// <param name="width">The width of the world.</param>
+		/// <param name="height">The height of the world.</param>
+		public static void UnserializeFromComplexObject(DatabaseArray worlddata, int width, int height)
+		{
+			Minimap minimap = new Minimap();
+			minimap.width = width;
+			minimap.height = height;
+			minimap.initialize();
 
-            minimap.initialize();
-            
+			Console.WriteLine("Unserializing complex object...");
 
-            List<Tuple<int, int, Color>> rewrittenBlocks = new List<Tuple<int, int, Color>>();
-            foreach (DatabaseObject ct in worlddata)
-            {
-                if (ct.Count == 0) continue;
-                var type = (uint)ct.GetValue("type");
-                var layerNum = ct.GetInt("layer", 0);
-                var xs = ct.GetBytes("x", new byte[0]);
-                var ys = ct.GetBytes("y", new byte[0]);
+			Color[,] foreground = new Color[width, height];
+			foreach (DatabaseObject ct in worlddata) {
+				if (ct.Count == 0) continue;
+				uint blockId = ct.GetUInt("type");
+				int layer = ct.GetInt("layer");
+				byte[] xs = ct.GetBytes("x");
+				byte[] ys = ct.GetBytes("y");
 
-                for (var b = 0; b < xs.Length; b += 2)
-                {
-                    var nx = (xs[b] << 8) + xs[b + 1];
-                    var ny = (ys[b] << 8) + ys[b + 1];
+				for (var b = 0; b < xs.Length; b += 2) {
+					int nx = (xs[b] << 8) | xs[b + 1];
+					int ny = (ys[b] << 8) | ys[b + 1];
 
-                    // if it can't draw it, it will draw it later.
-                    rewrittenBlocks.Add(minimap.drawBlock(new DataChunk(layerNum, type, xs, ys, new object[0]), new Point(nx,ny)));
-                }
-            }
+					minimap.drawBlock(layer, nx, ny, blockId);
+				}
+			}
 
-            // have to rewrite foreground blocks because they may have been written before
-            // the background blocks were.
+			// have to rewrite foreground blocks because they may have been written before
+			// the background blocks were.
 
-            minimap.rewriteForegroundBlocks(rewrittenBlocks);
+			minimap.rewriteForegroundBlocks();
 
-            minimap.Save(worldID + "_bigdb.png");
-            Console.WriteLine("Saved image");
-        }
+			minimap.Save(worldID + "_bigdb.png");
+			generating_minimap = false;
+		}
 
-        /// <summary>
-        /// Handles all incoming PlayerIO messages
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The message.</param>
-        static void Connection_OnMessage(object sender, Message e)
-        {
-            switch (e.Type)
-            {
-                case "init":
-                    globalConn.Disconnect(); // already have init data; don't need to be connected
+		/// <summary>
+		/// Handles all incoming PlayerIO messages
+		/// </summary>
+		/// <param name="sender">The sender.</param>
+		/// <param name="m">The message.</param>
+		static void Connection_OnMessage(object sender, Message m)
+		{
+			if (m.Type != "init")
+				return;
 
-                    List<Tuple<int, int, Color>> rewrittenBlocks = new List<Tuple<int, int, Color>>();
+			globalConn.Disconnect();
 
-                    Minimap minimap = new Minimap();
-                    minimap.width = e.GetInt(15);
-                    minimap.height = e.GetInt(16);
+			Minimap minimap = new Minimap();
+			minimap.width = m.GetInt(15);
+			minimap.height = m.GetInt(16);
 
-                    minimap.initialize();
-                    var chunks = InitParse.Parse(e);
-                    foreach (var chunk in chunks)
-                    {
-                        foreach (var pos in chunk.Locations)
-                        {  
-                            rewrittenBlocks.Add(
-                                minimap.drawBlock(chunk, pos));
-                        }
-                    }
+			minimap.initialize();
 
-                    minimap.rewriteForegroundBlocks(rewrittenBlocks);
+			uint p = 18;
+			while (m[p] as string != "ws") p++;
 
-                    minimap.Save(worldID + ".png");
-                    //pngCompressor();
+			// Parse world data
+			while (p < m.Count) {
+				uint blockId = m.GetUInt(p);
+				int layer = m.GetInt(p + 1);
+				byte[] ys = m.GetByteArray(p + 2);
+				byte[] xs = m.GetByteArray(p + 3);
 
-                    break;
-            }
-        }   
+				for (var b = 0; b < xs.Length; b += 2) {
+					int nx = (xs[b] << 8) | xs[b + 1];
+					int ny = (ys[b] << 8) | ys[b + 1];
 
-        /// <summary>
-        /// Compresses PNG files.
-        /// </summary>
-        private static void pngCompressor()
-        {
-            var quantizer = new WuQuantizer();
-            using (var bitmap = new Bitmap(worldID + ".png"))
-            {
-                using (var quantized = quantizer.QuantizeImage(bitmap, 0, 0))
-                {
-                    quantized.Save(worldID + "_nquant.png", ImageFormat.Png);
-                }
-            }
-        }
-    }
+					minimap.drawBlock(layer, nx, ny, blockId);
+				}
+
+				p += 4;
+
+				if (m[p] as string == "we")
+					break;
+
+				while (p < m.Count) {
+					if (m[p + 2] is byte[])
+						break;
+					p++;
+				}
+			}
+
+			minimap.rewriteForegroundBlocks();
+			minimap.Save(worldID + ".png");
+			generating_minimap = false;
+		}
+	}
 }
